b9b661fe4685c8db4106e1ec8569e652c242b178
monaji <monaji@LMA001081.corp.capgemini.com>
2018-07-25 22:01:05 +0100

    Add a simple output change

--- serinity/src/main/java/com/rup/serenity/App.java
+++ serinity/src/main/java/com/rup/serenity/App.java
@@ -4,10 +4,10 @@ package com.rup.serenity;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Allaho akbare");
+		System.out.println("first commit");
+	}
 }
